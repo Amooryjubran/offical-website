74ededd281b02f8b20bad0a2f101dd745ec04198
amooryjubran <omarsalehjubran@gmail.com>
2020-05-01 23:36:15 -0400

    context file

---
+++ src/components/Context/Context.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+
+import {Links} from '../Context/Data';
+import {Services} from '../Context/Data';
+import {Portfolio} from '../Context/Data';
+import {blogItems} from '../Context/Data';
+import {Interiors} from '../Context/Data';
+import {Events} from '../Context/Data';
+
+
+
+
+
+// 1- 
+const ProductContext = React.createContext();
+
+
+
+
+
+
+class ProductProvider extends Component {
+    state = {
+        sidebarOpen: false,
+        closeLinks: false,
+        links: links,
+        services: Services,
+        portfolio: Portfolio,
+        headerShow: false,
+        blogItems: blogItems,
+        Interios: Interiors,
+        Events: Events
+
+    }
+    render() {
+        return (
+            <ProductContext.Provider value="hello"> 
+                {this.props.children}
+            </ProductContext.Provider>
+        )
+    }
+}
+
+const ProductConsumer = ProductContext.Consumer;
+
+export {ProductProvider, ProductConsumer}
