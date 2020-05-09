f94e87bb7a865a07fab3d774af05a022f6662a59
amooryjubran <omarsalehjubran@gmail.com>
2020-05-09 00:26:13 -0400

    contact page

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import ContactPage from '../components/ContactPage/ContactPage'
 
 export default function Contact() {
     return (
         <div>
-             <h1>Hello from Contact</h1>
+             <ContactPage />
         </div>
     )
 }
