c7e9835f2d8636f6f90462dae0d66d8119d9d5e1
amooryjubran <omarsalehjubran@gmail.com>
2020-05-09 00:28:32 -0400

    inspiration page --

--- src/Pages/Inspirations.js
+++ src/Pages/Inspirations.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import InspirationsPage from '../components/InspirationsPage/InspirationsPage'
 
 export default function Inspirations() {
     return (
         <div>
-             <h1>Hello from Inspirations</h1>
+             <InspirationsPage />
         </div>
     )
 }
