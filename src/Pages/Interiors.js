7212f0eba72334031f542342945df05c70fb5667
amooryjubran <omarsalehjubran@gmail.com>
2020-05-08 23:44:02 -0400

    fixesses

--- src/Pages/Interiors.js
+++ src/Pages/Interiors.js
@@ -4,7 +4,7 @@ import InteriorsPage from '../components/InteriorsPage/InteriorsPage'
 export default function Interiors() {
     return (
         <div>
-           <h1>ea</h1>
+           <InteriorsPage/>
         </div>
     )
 }
